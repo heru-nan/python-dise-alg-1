88dd2e62c3dc81d53c3d8b6d596bf445d36770e0
heru-nan <hrwgallardo@gmail.com>
2022-05-24 18:23:40 -0400

    add shannon fano to compression script

--- encode.py
+++ encode.py
@@ -6,23 +6,30 @@ from bitarray import bitarray
 def main():
     print(sys.argv)
     script = sys.argv[0]
-    filename = sys.argv[1]
+    flag = sys.argv[1]
+    filename = sys.argv[2]
+
+    type_of_compression = 'h'
+    if(flag == "s"):
+        type_of_compression = "s"
+
     txt = Path('./data/' + filename).read_text(encoding='Latin-1')
 
     print("Archivo cargado, ", get_size(txt))
     
     # tiempo 1
-    dendograma = huffman(txt)
+    dendograma = huffman(txt) if type_of_compression == "h" else shannon_fano(txt)
     # tiempo 1
     
     # tiempo 2
+
     dendograma_bitarray = {k: bitarray(v) for k, v in dendograma.items()}
     bits = bitarray()
     bits.encode(dendograma_bitarray, txt)
     # tiempo 2
 
     # tiempo 3
-    with open('./data/encode/' + "s_" + filename, 'wb') as fh:
+    with open('./data/encode/' + type_of_compression + "_" + filename, 'wb') as fh:
         bits.tofile(fh)
     # tiempo 3
 
--- functions/shannon_fano.py
+++ functions/shannon_fano.py
@@ -1,6 +1,7 @@
+import collections
+import heapq
 
-
-def shannon_fano(probs, beg, end):
+def part_shannon_fano(probs, beg, end):
     if(beg == end): return
     y = beg
     z = end
@@ -18,5 +19,46 @@ def shannon_fano(probs, beg, end):
     for h in range(y, end + 1):
         probs[h][1][1] =  probs[h][1][1] + "0"
     
-    sf(probs, beg, y-1)
-    sf(probs, y, end)
+    part_shannon_fano(probs, beg, y-1)
+    part_shannon_fano(probs, y, end)
+
+def partition(array, low, high):
+  # Choose the rightmost element as pivot
+  pivot = array[high][0]
+  # Pointer for greater element
+  i = low - 1
+  # Traverse through all elements
+  # compare each element with pivot
+  for j in range(low, high):
+    if (array[j][0]) <= pivot:
+      # If element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+      # Swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+  # Swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+  # Return the position from where partition is done
+  return i + 1
+ 
+# Function to perform quicksort
+def quick_sort(array, low, high):
+  if low < high:
+    # Find pivot element such that
+    # element smaller than pivot are on the left
+    # element greater than pivot are on the right
+    pi = partition(array, low, high)
+    # Recursive call on the left of pivot
+    quick_sort(array, low, pi-1)
+    # Recursive call on the right of pivot
+    quick_sort(array, pi + 1, high)
+      
+
+
+def shannon_fano(toEncode):
+    arr = toEncode
+    dendograma = [[frequencia/len(arr), [simbolo, ""]] for simbolo, frequencia in collections.Counter(arr).items()]
+    quick_sort(dendograma,0,len(dendograma)-1)
+    part_shannon_fano(dendograma, 0, len(dendograma)-1)
+    dendograma = {codigos[0] : codigos[1] for prob, codigos in dendograma}
+    return dendograma
