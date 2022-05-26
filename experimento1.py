dbace7ed9c3e8a86bc90938fd0496ad9f0aeea0c
JpPezoM <juan.pablo.pzm@gmail.com>
2022-05-25 23:03:37 -0400

    tiempos del experimento1

--- experimento1.py
+++ experimento1.py
@@ -2,36 +2,33 @@ import sys
 from pathlib import Path
 from functions.index import *
 import random
-
-def Experimento1(toEncode,intentos):
-    for i in range(intentos):
-        k=random.randint(0,len(toEncode))
-        porcion=""
-        for j in range(0,k):
-            porcion+=toEncode[j]
-        dendogramaSf=shannon_fano(porcion)
-        print("dendogramaSf realizado y su tamaño es:",get_size(dendogramaSf))
-        dendogramaH=huffman(porcion)
-        print("dendogramaH realizado y su tamaño es:",get_size(dendogramaH))
-
+from time import time
 
 def main():
     print(sys.argv)
     script = sys.argv[0]
     flags=sys.argv[1]
     filename = sys.argv[2]
-    txt = Path('./data/' + filename).read_text(encoding='Latin-1')
-    print("Archivo cargado, ", get_size(txt))
 
-    for i in range(flags):
+    txt = Path('./data/' + filename).read_text(encoding='Latin-1')
+
+    for i in range(int(flags)):
         k=random.randint(0,len(txt))
         porcion=txt[0:k]
+        print("intento ",i," de Experimento 1 para la procion T[0...",k,"]:")
         #for j in range(0,k):
         #    porcion+=txt[j]
+        shannon_time_1 =time()
         dendogramaSf=shannon_fano(porcion)
-        print("dendogramaSf realizado y su tamaño es:",get_size(dendogramaSf))
+        shannon_time_2 =time()
+        
+        huffman_time_1 =time()
         dendogramaH=huffman(porcion)
-        print("dendogramaH realizado y su tamaño es:",get_size(dendogramaH))
+        huffman_time_2 =time()
+        
+        print("Tiempo de Extraccion de prefijos utilizando Shannon: ", shannon_time_2-shannon_time_1, " segundos")
+        print("Tiempo de Extraccion de prefijos utilizando Huffman: ", huffman_time_2-huffman_time_1, " segundos")
+        print()
 
     
 
